fc204ecfc2a46fd8f98ee83770d80e0456b713be
foreversuccess <shuidianzhan2007@163.com>
2018-02-28 18:02:46 +0800

    完成

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -2,13 +2,12 @@ package com.coolweather.android.db;
 
 import org.litepal.crud.DataSupport;
 
-/**
- * Created by JohnSmith on 2018/2/27.
- */
-
 public class Province extends DataSupport {
+
     private int id;
+
     private String provinceName;
+
     private int provinceCode;
 
     public int getId() {
